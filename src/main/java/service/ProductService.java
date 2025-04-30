1b70e45251514d758433a23bf3432928df27a1fd
pdrotmz <pedrotomaz0210@gmail.com>
2025-04-30 16:02:34 -0300

    feat: adds exceptions implementations

--- src/main/java/service/ProductService.java
+++ src/main/java/service/ProductService.java
@@ -1,8 +1,10 @@
 package service;
 
 import dao.ProductDAO;
+import exceptions.product.InvalidValuePrice;
 import model.product.Product;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 
@@ -12,6 +14,9 @@ public class ProductService {
     // TODO: Implements some logic to registerProduct
 
     public void registerProduct(Product product) {
+        if (product.getPrice().compareTo(BigDecimal.ZERO) < 0) {
+            throw new InvalidValuePrice();
+        }
         productDAO.saveProduct(product);
     }
 
--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,13 +1,23 @@
 package service;
 
 import dao.UserDAO;
+import exceptions.user.InvalidEmailFormatException;
+import exceptions.user.InvalidPasswordFormat;
 import model.user.User;
+import util.EncryptPassword;
 
 public class UserService {
 
     private static final UserDAO userDao = new UserDAO();
 
     public void registerUser(User user) {
+        if(!user.getEmail().contains("@")) {
+            throw new InvalidEmailFormatException(user.getEmail());
+        } else if (!(user.getPassword().length() > 8)) {
+            throw new InvalidPasswordFormat();
+        }
+        String hashedPassword = EncryptPassword.hashPassword(user.getPassword());
+        user.setPassword(hashedPassword);
         userDao.saveUser(user);
     }
 
