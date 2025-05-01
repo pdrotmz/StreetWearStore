b494948ad4ba479fe90ee278efc9ea472fe4ffb6
pdrotmz <pedrotomaz0210@gmail.com>
2025-05-01 19:51:37 -0300

    feat: adds some spaghetti code to run this all hahaha

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,30 +1,96 @@
-import dao.ProductDAO;
+import model.admin.Admin;
 import model.product.Product;
 import model.user.User;
+import service.AdminService;
 import service.ProductService;
 import service.UserService;
 
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     private static final ProductService productService = new ProductService();
     private static final UserService userService = new UserService();
+    private static final AdminService adminService = new AdminService();
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        boolean execution = true;
         User user = new User();
+        Admin admin = new Admin();
 
-        System.out.println("----- FAÇA SEU CADASTRO -----");
-        System.out.print("\nDigite seu username: ");
-        user.setUsername(scanner.nextLine());
-        System.out.print("\nDigite seu email: ");
-        user.setEmail(scanner.nextLine());
-        System.out.print("\nDigite sua senha: ");
-        user.setPassword(scanner.nextLine());
+        boolean execution = true;
+        int optionNotLogged, optionRole;
 
-        userService.registerUser(user);
+        System.out.println("------------- BEM-VINDOS A STREET WEAR STORE -------------");
+        System.out.println("Qual o seu papel: ");
+        System.out.println("1 - CLIENTE");
+        System.out.println("2 - ADMIN");
+        optionRole = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (optionRole) {
+            case 1 -> {
+                System.out.println("------------- BEM-VINDOS A STREET WEAR STORE -------------");
+                System.out.println("Primeiro, escolha uma das opções: ");
+                System.out.println("1 - FAZER CADASTRO");
+                System.out.println("2 - FAZER LOGIN");
+                optionNotLogged = scanner.nextInt();
+                scanner.nextLine();
+
+                switch (optionNotLogged) {
+                    case 1 -> {
+                        System.out.println("----- FAÇA SEU CADASTRO -----");
+                        System.out.print("\nDigite seu username: ");
+                        user.setUsername(scanner.nextLine());
+                        System.out.print("\nDigite seu email: ");
+                        user.setEmail(scanner.nextLine());
+                        System.out.print("\nDigite sua senha: ");
+                        user.setPassword(scanner.nextLine());
+
+                        userService.registerUser(user);
+                    }
+
+                    case 2 -> {
+                        System.out.println("----- FAÇA SEU LOGIN -----");
+                        System.out.print("Digite o seu username: ");
+                        user.setUsername(scanner.nextLine());
+                        System.out.print("Digite sua senha: ");
+                        user.setPassword(scanner.nextLine());
+                        userService.loginUser(user.getUsername(), user.getPassword());
+                    }
+                    default -> System.out.println("Opção inválida!");
+                }
+
+            }
+
+            case 2 -> {
+
+                System.out.println("------------- BEM-VINDOS A STREET WEAR STORE -------------");
+                System.out.println("Primeiro, escolha uma das opções: ");
+                System.out.println("1 - FAZER CADASTRO");
+                System.out.println("2 - FAZER LOGIN");
+                optionNotLogged = scanner.nextInt();
+                scanner.nextLine();
+
+
+                if (optionNotLogged == 1) {
+                    System.out.println("----- FAÇA SEU CADASTRO -----");
+                    System.out.print("\nDigite seu username: ");
+                    admin.setUsername(scanner.nextLine());
+                    System.out.print("\nDigite seu email: ");
+                    admin.setEmail(scanner.nextLine());
+                    System.out.println("\nDigite seu domínio: ");
+                    admin.setDomain(scanner.nextLine());
+                    System.out.print("\nDigite sua senha: ");
+                    admin.setPassword(scanner.nextLine());
+
+                    adminService.registerAdmin(admin);
+                }
+            }
+        }
+
+
+
+
 
         while(execution) {
             System.out.println("------------- BEM-VINDOS A STREET WEAR STORE -------------");
