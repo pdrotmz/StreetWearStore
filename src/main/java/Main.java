c1191e9cdfbb114eed46d4c69d17990fb7c4b4e4
pdrotmz <pedrotomaz0210@gmail.com>
2025-05-02 03:52:53 -0300

    refactor: applies all changes and organize the code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import controller.AdminController;
+import controller.UserController;
 import model.admin.Admin;
 import model.product.Product;
 import model.user.User;
@@ -9,16 +11,14 @@ import java.util.Scanner;
 
 public class Main {
     private static final ProductService productService = new ProductService();
-    private static final UserService userService = new UserService();
-    private static final AdminService adminService = new AdminService();
+    private static final UserController userController = new UserController();
+    private static final AdminController adminController = new AdminController();
+
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        User user = new User();
-        Admin admin = new Admin();
-
         boolean execution = true;
-        int optionNotLogged, optionRole;
+        int optionRole;
 
         System.out.println("------------- BEM-VINDOS A STREET WEAR STORE -------------");
         System.out.println("Qual o seu papel: ");
@@ -29,103 +29,12 @@ public class Main {
 
         switch (optionRole) {
             case 1 -> {
-                System.out.println("------------- BEM-VINDOS A STREET WEAR STORE -------------");
-                System.out.println("Primeiro, escolha uma das opções: ");
-                System.out.println("1 - FAZER CADASTRO");
-                System.out.println("2 - FAZER LOGIN");
-                optionNotLogged = scanner.nextInt();
-                scanner.nextLine();
-
-                switch (optionNotLogged) {
-                    case 1 -> {
-                        System.out.println("----- FAÇA SEU CADASTRO -----");
-                        System.out.print("\nDigite seu username: ");
-                        user.setUsername(scanner.nextLine());
-                        System.out.print("\nDigite seu email: ");
-                        user.setEmail(scanner.nextLine());
-                        System.out.print("\nDigite sua senha: ");
-                        user.setPassword(scanner.nextLine());
-
-                        userService.registerUser(user);
-                    }
-
-                    case 2 -> {
-                        System.out.println("----- FAÇA SEU LOGIN -----");
-                        System.out.print("Digite o seu username: ");
-                        user.setUsername(scanner.nextLine());
-                        System.out.print("Digite sua senha: ");
-                        user.setPassword(scanner.nextLine());
-                        userService.loginUser(user.getUsername(), user.getPassword());
-                    }
-                    default -> System.out.println("Opção inválida!");
-                }
-
+                userController.menu();
             }
 
             case 2 -> {
-
-                System.out.println("------------- BEM-VINDOS A STREET WEAR STORE -------------");
-                System.out.println("Primeiro, escolha uma das opções: ");
-                System.out.println("1 - FAZER CADASTRO");
-                System.out.println("2 - FAZER LOGIN");
-                optionNotLogged = scanner.nextInt();
-                scanner.nextLine();
-
-
-                if (optionNotLogged == 1) {
-                    System.out.println("----- FAÇA SEU CADASTRO -----");
-                    System.out.print("\nDigite seu username: ");
-                    admin.setUsername(scanner.nextLine());
-                    System.out.print("\nDigite seu email: ");
-                    admin.setEmail(scanner.nextLine());
-                    System.out.println("\nDigite seu domínio: ");
-                    admin.setDomain(scanner.nextLine());
-                    System.out.print("\nDigite sua senha: ");
-                    admin.setPassword(scanner.nextLine());
-
-                    adminService.registerAdmin(admin);
-                }
-            }
-        }
-
-
-
-
-
-        while(execution) {
-            System.out.println("------------- BEM-VINDOS A STREET WEAR STORE -------------");
-            System.out.println("\n1- Cadastrar Produto");
-            System.out.println("2 - Lista Produtos");
-            System.out.println("3 - Sair");
-            System.out.println();
-
-            int option = scanner.nextInt();
-            scanner.nextLine();
-
-            switch (option) {
-                case 1 -> {
-                    Product product = new Product();
-                    System.out.print("Digite um nome para o produto: ");
-                    product.setName(scanner.nextLine());
-                    System.out.print("Digite uma descrição para o produto: ");
-                    product.setDescription(scanner.nextLine());
-                    System.out.print("Digite um valor para o produto: ");
-                    product.setPrice(scanner.nextBigDecimal());
-                    System.out.print("Digite a quantidade recebida: ");
-                    product.setQuantity(scanner.nextInt());
-                    scanner.nextLine();
-
-
-                    productService.registerProduct(product);
-                    System.out.println();
-                    System.out.println("Produto cadastrado com sucesso");
-                }
-                case 2 -> {
-                    var products = productService.findAllProducts();
-                    products.forEach(product -> System.out.println(product.getName() + " - " + product.getPrice() + "\n"));
-                }
-                case 3 -> execution = false;
-                default -> System.out.println("Opção inválida!");
+                adminController.menu();
+                adminController.painelAdmin();
             }
         }
     }
