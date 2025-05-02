ada779f91207471e40c94b5b77587e89053d44a0
pdrotmz <pedrotomaz0210@gmail.com>
2025-05-02 03:50:19 -0300

    fix: fixes bugs password null

--- src/main/java/dao/AdminDAO.java
+++ src/main/java/dao/AdminDAO.java
@@ -12,15 +12,15 @@ import java.sql.SQLException;
 public class AdminDAO {
 
     public void SaveAdmin(Admin admin) {
-        String sql = "INSERT INTO admin (username, email, domain, password) VALUES (?, ?, ?, ?)";
+        String sql = "INSERT INTO admin (username, email, password, domain) VALUES (?, ?, ?, ?)";
 
-        try(Connection connection = ConnectionFactory.getConnection();
+        try(Connection connection = ConnectionFactory.getInstance();
             PreparedStatement statement = connection.prepareStatement(sql)) {
 
             statement.setString(1, admin.getUsername());
             statement.setString(2, admin.getEmail());
-            statement.setString(4, admin.getDomain());
             statement.setString(3, admin.getPassword());
+            statement.setString(4, admin.getDomain());
 
             ResultSet resultSet = statement.executeQuery();
         } catch(SQLException exception) {
@@ -32,7 +32,7 @@ public class AdminDAO {
 
         String sql = "SELECT * FROM admin WHERE email = ?";
 
-        try(Connection connection = ConnectionFactory.getConnection();
+        try(Connection connection = ConnectionFactory.getInstance();
             PreparedStatement statement = connection.prepareStatement(sql)) {
 
             statement.setString(1, email);
@@ -43,8 +43,8 @@ public class AdminDAO {
                 admin.setId(resultSet.getLong("id"));
                 admin.setUsername(resultSet.getString("username"));
                 admin.setEmail(resultSet.getString("email"));
-                admin.setDomain(resultSet.getString("domain"));
                 admin.setPassword(resultSet.getString("password"));
+                admin.setDomain(resultSet.getString("domain"));
 
                 return admin;
             }
