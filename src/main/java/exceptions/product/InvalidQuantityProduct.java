d418d5d3668b134d9f640a9b44d6bf38a18c1adc
pdrotmz <pedrotomaz0210@gmail.com>
2025-05-04 21:28:08 -0300

    refactor: removes something
    - Removes Category enum
    - Removes InvalidQuantityProduct
    - Removes InvalidValuePrice

--- src/main/java/exceptions/product/InvalidQuantityProduct.java
+++
@@ -1,7 +0,0 @@
-package exceptions.product;
-
-public class InvalidQuantityProduct extends RuntimeException {
-  public InvalidQuantityProduct() {
-    super("A quantity de produtos adicionados deve ser maior que zero.");
-  }
-}
--- src/main/java/exceptions/product/InvalidValuePrice.java
+++
@@ -1,7 +0,0 @@
-package exceptions.product;
-
-public class InvalidValuePrice extends RuntimeException {
-    public InvalidValuePrice() {
-        super("O preço do produto deve ser maior que zero.");
-    }
-}
